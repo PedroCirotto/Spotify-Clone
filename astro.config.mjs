3be3f498d70fee16ff1ef2961f611c6e242b61c5
PedroCirotto <pedro@peakonedev.com>
2024-03-22 22:15:07 -0400

    Deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://astronaut.github.io',
+  site: 'https://pedrocirotto.github.io/Spotify-Clone/',
   base: 'spotify',
 });
